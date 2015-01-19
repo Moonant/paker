5a4f885d40f09f562a30f1da72895c2b782a41fa
haoranzhang <1977123321@qq.com>
2015-01-19 23:25:22 +0800

    haoran chg tab and intent

--- server/alg/individule.js
+++ server/alg/individule.js
@@ -1,219 +1,219 @@
 var Individule = function (pConfig, pCourses, pCoursesIds, pCoursesArranged, pTeacherIds, pClassesIds) {
 
-    this.chomesome = "";
-    this.fitness = 0;
+  this.chomesome = "";
+  this.fitness = 0;
 
-    this.pConfig = pConfig;
-    this.pCourses = pCourses;
-    this.pCoursesIds = pCoursesIds;
-    this.pCoursesArranged = pCoursesArranged;
-    this.pTeacherIds = pTeacherIds;
-    this.pClassesIds = pClassesIds;
+  this.pConfig = pConfig;
+  this.pCourses = pCourses;
+  this.pCoursesIds = pCoursesIds;
+  this.pCoursesArranged = pCoursesArranged;
+  this.pTeacherIds = pTeacherIds;
+  this.pClassesIds = pClassesIds;
 
 
-    this.timeTable = [];
-    this.teacherTables = [];
-    this.classesTables = [];
-    this.coursesTables = [];
+  this.timeTable = [];
+  this.teacherTables = [];
+  this.classesTables = [];
+  this.coursesTables = [];
 
-    this.conflicts;
+  this.conflicts;
 
-    this.initTables();
+  this.initTables();
 };
 
 Individule.prototype.setRandomChomesome = function () {
-    for (var i = 0; i < this.pConfig.geneNum; i++) {
-        var item = {};
-        item.dayId = parseInt(this.pConfig.pday * Math.random());
-        item.timeId = parseInt(this.pConfig.ptime * Math.random());
-        this.timeTable.push(item);
-    }
+  for (var i = 0; i < this.pConfig.geneNum; i++) {
+    var item = {};
+    item.dayId = parseInt(this.pConfig.pday * Math.random());
+    item.timeId = parseInt(this.pConfig.ptime * Math.random());
+    this.timeTable.push(item);
+  }
 
-    this.enCode();
+  this.enCode();
 };
 
 Individule.prototype.enCode = function () {
-    this.chomesome = "";
-    for (var i in this.timeTable) {
-        var c = this.timeTable[i].dayId * this.pConfig.ptime + this.timeTable[i].timeId;
-        this.chomesome += this.padStr(c.toString(2), this.pConfig.genuLength);
-    }
+  this.chomesome = "";
+  for (var i in this.timeTable) {
+    var c = this.timeTable[i].dayId * this.pConfig.ptime + this.timeTable[i].timeId;
+    this.chomesome += this.padStr(c.toString(2), this.pConfig.genuLength);
+  }
 };
 
 Individule.prototype.deCode = function () {
-    this.timeTable = [];
-    for (var i = 0; i < this.pConfig.geneNum; i++) {
-        var item = {};
-        var gene = this.chomesome.substr(i * this.pConfig.genuLength, this.pConfig.genuLength);
-        var c = parseInt(gene, 2);
+  this.timeTable = [];
+  for (var i = 0; i < this.pConfig.geneNum; i++) {
+    var item = {};
+    var gene = this.chomesome.substr(i * this.pConfig.genuLength, this.pConfig.genuLength);
+    var c = parseInt(gene, 2);
 
-        if (c >= this.pConfig.pday * this.pConfig.ptime) {
-            this.mutateSingleBit(i * this.pConfig.genuLength);
-            c = parseInt(gene.substr(1, this.pConfig.genuLength-1), 2);
-        }
-
-        item.dayId = parseInt(c / this.pConfig.ptime);
-        item.timeId = parseInt(c % this.pConfig.ptime);
-        this.timeTable.push(item);
+    if (c >= this.pConfig.pday * this.pConfig.ptime) {
+      this.mutateSingleBit(i * this.pConfig.genuLength);
+      c = parseInt(gene.substr(1, this.pConfig.genuLength - 1), 2);
     }
+
+    item.dayId = parseInt(c / this.pConfig.ptime);
+    item.timeId = parseInt(c % this.pConfig.ptime);
+    this.timeTable.push(item);
+  }
 };
 
 Individule.prototype.getInitTable = function () {
-    var r = [];
-    for (var i = 0; i < this.pConfig.pday; i++) {
-        r[i] = [];
-        for (var j = 0; j < this.pConfig.ptime; j++) {
-            r[i][j] = '-';
-        }
+  var r = [];
+  for (var i = 0; i < this.pConfig.pday; i++) {
+    r[i] = [];
+    for (var j = 0; j < this.pConfig.ptime; j++) {
+      r[i][j] = '-';
     }
-    return r;
+  }
+  return r;
 };
 Individule.prototype.clearTable = function (r) {
-    for (var i = 0; i < this.pConfig.pday; i++) {
-        for (var j = 0; j < this.pConfig.ptime; j++) {
-            r[i][j] = '-';
-        }
+  for (var i = 0; i < this.pConfig.pday; i++) {
+    for (var j = 0; j < this.pConfig.ptime; j++) {
+      r[i][j] = '-';
     }
+  }
 };
 
 Individule.prototype.initTables = function () {
-    for (var i = 0; i < this.pTeacherIds.length; i++) {
-        this.teacherTables[i] = this.getInitTable();
-    }
-    for (i = 0; i < this.pClassesIds.length; i++) {
-        this.classesTables[i] = this.getInitTable();
-    }
+  for (var i = 0; i < this.pTeacherIds.length; i++) {
+    this.teacherTables[i] = this.getInitTable();
+  }
+  for (i = 0; i < this.pClassesIds.length; i++) {
+    this.classesTables[i] = this.getInitTable();
+  }
 
-    this.coursesTables = [];
+  this.coursesTables = [];
 };
 Individule.prototype.clearTable = function (r) {
-    for (var i = 0; i < this.pConfig.pday; i++) {
-        for (var j = 0; j < this.pConfig.ptime; j++) {
-            r[i][j] = '-';
-        }
+  for (var i = 0; i < this.pConfig.pday; i++) {
+    for (var j = 0; j < this.pConfig.ptime; j++) {
+      r[i][j] = '-';
     }
+  }
 };
 Individule.prototype.clearTables = function () {
-    for (var i = 0; i < this.pTeacherIds.length; i++) {
-        this.clearTable(this.teacherTables[i]);
-    }
-    for (i = 0; i < this.pClassesIds.length; i++) {
-        this.clearTable(this.classesTables[i]);
-    }
+  for (var i = 0; i < this.pTeacherIds.length; i++) {
+    this.clearTable(this.teacherTables[i]);
+  }
+  for (i = 0; i < this.pClassesIds.length; i++) {
+    this.clearTable(this.classesTables[i]);
+  }
 
-    var course;
-    for (i in this.pCoursesArranged) {
-        course = this.pCoursesArranged[i];
-        for (var j in course.ptime) {
-            this.teacherTables[course.teacherId][course.ptime[j].dayId][course.ptime[j].timeId] = course.courseId;
-            for (var z in course.classIds) {
-                this.classesTables[course.classIds[z]][course.ptime[j].dayId][course.ptime[j].timeId] = course.courseId;
-            }
+  var course;
+  for (i in this.pCoursesArranged) {
+    course = this.pCoursesArranged[i];
+    for (var j in course.ptime) {
+      this.teacherTables[course.teacherId][course.ptime[j].dayId][course.ptime[j].timeId] = course.courseId;
+      for (var z in course.classIds) {
+        this.classesTables[course.classIds[z]][course.ptime[j].dayId][course.ptime[j].timeId] = course.courseId;
+      }
 
-        }
     }
+  }
 
-    this.coursesTables = [];
+  this.coursesTables = [];
 };
 
 
 Individule.prototype.isConflict = function (obj, x, y, z, courseId) {
-    if (obj[x][y][z] != '-') {
-        obj[x][y][z] = obj[x][y][z] + "-" + courseId;
-        return true;
-    } else {
-        obj[x][y][z] = '' + courseId;
-        return false;
-    }
+  if (obj[x][y][z] != '-') {
+    obj[x][y][z] = obj[x][y][z] + "-" + courseId;
+    return true;
+  } else {
+    obj[x][y][z] = '' + courseId;
+    return false;
+  }
 };
 
 Individule.prototype.mutateSingleBit = function (i) {
-    var c = this.chomesome.charAt(i);
-    if (c == '1') {
-        this.chomesome = this.chomesome.substring(0, i) + '0' + this.chomesome.substring(i + 1, this.pConfig.chromeLength);
-    } else {
-        this.chomesome = this.chomesome.substring(0, i) + '1' + this.chomesome.substring(i + 1, this.pConfig.chromeLength);
-    }
+  var c = this.chomesome.charAt(i);
+  if (c == '1') {
+    this.chomesome = this.chomesome.substring(0, i) + '0' + this.chomesome.substring(i + 1, this.pConfig.chromeLength);
+  } else {
+    this.chomesome = this.chomesome.substring(0, i) + '1' + this.chomesome.substring(i + 1, this.pConfig.chromeLength);
+  }
 };
 
 Individule.prototype.pad = function (num, n) {
-    var len = num.toString().length;
-    while (len < n) {
+  var len = num.toString().length;
+  while (len < n) {
 
-        num = "0" + num;
-        len++;
-    }
+    num = "0" + num;
+    len++;
+  }
 
-    return num;
+  return num;
 };
 
 Individule.prototype.padStr = function (str, n) {
-    var len = str.length;
-    while (len < n) {
+  var len = str.length;
+  while (len < n) {
 
-        str = "0" + str;
-        len++;
-    }
+    str = "0" + str;
+    len++;
+  }
 
-    return str;
+  return str;
 };
 
 Individule.prototype.calFitness = function () {
-    this.deCode();
-    this.clearTables();
+  this.deCode();
+  this.clearTables();
 
-    var tea = 0, clas = 0, clasDay = 0, night = 0;
+  var tea = 0, clas = 0, clasDay = 0, night = 0;
 
-    for (var i = 0; i < this.pConfig.geneNum; i++) {
-        var courseId = this.pCoursesIds[i];
-        var teacherId = this.pCourses[courseId].teacherId;
-        var classesIds = this.pCourses[courseId].classesIds;
-        var dayId = this.timeTable[i].dayId;
-        var timeId = this.timeTable[i].timeId;
+  for (var i = 0; i < this.pConfig.geneNum; i++) {
+    var courseId = this.pCoursesIds[i];
+    var teacherId = this.pCourses[courseId].teacherId;
+    var classesIds = this.pCourses[courseId].classesIds;
+    var dayId = this.timeTable[i].dayId;
+    var timeId = this.timeTable[i].timeId;
 
-        if(timeId>3){
-            night++;
-        }
-
-        if (this.isConflict(this.teacherTables, teacherId, dayId, timeId, courseId)) {
-            tea++;
-        }
-
-        for (var j in classesIds) {
-            if (this.isConflict(this.classesTables, classesIds[j], dayId, timeId, courseId)) {
-                clas++;
-            }
-        }
-
-        if (this.coursesTables[courseId]) {
-            var t = {};
-            t.dayId = dayId;
-            t.timeId = timeId;
-
-            for (var j in this.coursesTables[courseId]) {
-                var day = Math.abs(this.coursesTables[courseId][j].dayId - t.dayId);
-                if (day == 0) {
-                    clasDay += 2;
-                } else if (day == 1) {
-                    clasDay++;
-                }
-            }
-
-            this.coursesTables[courseId].push(t);
-
-        } else {
-            this.coursesTables[courseId] = [];
-            this.coursesTables[courseId][0] = {};
-            this.coursesTables[courseId][0].dayId = dayId;
-            this.coursesTables[courseId][0].timeId = timeId;
-        }
+    if (timeId > 3) {
+      night++;
     }
 
-    this.fitness = 1 / (1 + 2 * tea + 2 * clas + 0.1 * clasDay + 0.02*night);
+    if (this.isConflict(this.teacherTables, teacherId, dayId, timeId, courseId)) {
+      tea++;
+    }
 
-    this.conflicts = [tea, clas, clasDay,night];
+    for (var j in classesIds) {
+      if (this.isConflict(this.classesTables, classesIds[j], dayId, timeId, courseId)) {
+        clas++;
+      }
+    }
 
-    return this.fitness;
+    if (this.coursesTables[courseId]) {
+      var t = {};
+      t.dayId = dayId;
+      t.timeId = timeId;
+
+      for (var j in this.coursesTables[courseId]) {
+        var day = Math.abs(this.coursesTables[courseId][j].dayId - t.dayId);
+        if (day == 0) {
+          clasDay += 2;
+        } else if (day == 1) {
+          clasDay++;
+        }
+      }
+
+      this.coursesTables[courseId].push(t);
+
+    } else {
+      this.coursesTables[courseId] = [];
+      this.coursesTables[courseId][0] = {};
+      this.coursesTables[courseId][0].dayId = dayId;
+      this.coursesTables[courseId][0].timeId = timeId;
+    }
+  }
+
+  this.fitness = 1 / (1 + 2 * tea + 2 * clas + 0.1 * clasDay + 0.02 * night);
+
+  this.conflicts = [tea, clas, clasDay, night];
+
+  return this.fitness;
 };
 
 module.exports = Individule;
--- server/xlsx/xlsx.js
+++ server/xlsx/xlsx.js
@@ -1,61 +1,60 @@
 var xlsx = require('node-xlsx');
 var express = require('express');
+var mongoose = require('mongoose');
 var Course = require('../models/course');
 var router = express.Router();
 
 
 function parseXlsx(filename) {
-    var result = {};
-    try {
-        result.obj = xlsx.parse(__dirname + '/plan2.xlsx'); // parses a file
-        addCoursesToDB(result.obj);
-        result.status = true;
-    } catch (e) {
-        result.status = false;
-        if (result.msg)
-            result.msg = "文件格式应该为.xlsx";
-    }
-    return result;
+  var result = {};
+  try {
+    result.obj = xlsx.parse(__dirname + '/plan.xlsx'); // parses a file
+    addCoursesToDB(result);
+    result.status = true;
+  } catch (e) {
+    result.status = false;
+    if (result.msg)
+      result.msg = "文件格式应该为.xlsx";
+  }
+  return result;
 }
 
-function parseCourses(obj) {
-
-
-}
-
-function addCoursesToDB(obj) {
-    var connStr = 'mongodb://localhost:27017/packer';
-    mongoose.connect(connStr, function (err) {
-        if (err) console.log('delete major' + err);
-    });
-    try {
-        for (var i = 3; i < obj.data.length - 2; i++) {
-            var c = obj.data[i];
-            var newCourse = new Course();
-            newCourse.name = c[0];
-            newCourse.classesName = c[0];
-            newCourse.type = {};
-            newCourse.type.isCompulsory = (c[1] == "必修");
-            newCourse.type.name = c[2];
-            newCourse.onlineHours = c[5];
-            newCourse.lectureHours = c[4];
-            newCourse.intermHours = c[8];
-            newCourse.labHours = c[6];
-            newCourse.extracurricular = c[7];
-            newCourse.totalHours = c[3];
-            newCourse.save();
-        }
-    } catch (e) {
-        console.log(e);
+function addCoursesToDB(result) {
+  var connStr = 'mongodb://localhost:27017/packer';
+  mongoose.connect(connStr, function (err) {
+    if (err) console.log('delete major' + err);
+  });
+  var obj = result.obj[0];
+  try {
+    for (var i = 3; i < obj.data.length - 2; i++) {
+      var c = obj.data[i];
+      var newCourse = new Course();
+      newCourse.name = c[0];
+      newCourse.classesName = c[0];
+      newCourse.type = {};
+      newCourse.type.isCompulsory = (c[1] == "必修");
+      newCourse.type.name = c[2];
+      newCourse.onlineHours = c[5];
+      newCourse.lectureHours = c[4];
+      newCourse.intermHours = c[8];
+      newCourse.labHours = c[6];
+      newCourse.extracurricular = c[7];
+      newCourse.totalHours = c[3];
+      newCourse.save();
     }
+  } catch (e) {
+    console.log(e);
+    result.msg = "文件内排版有误";
+    result.status = false;
+  }
 
-    mongoose.connection.close();
+  mongoose.connection.close();
 }
 
 
 // Test
 router.get('/xlsx', function (req, res) {
-    res.send(parseXlsx());
+  res.send(parseXlsx());
 });
 
 //var obj = xlsx.parse(fs.readFileSync(__dirname + '/myFile.xlsx')); // parses a buffer
